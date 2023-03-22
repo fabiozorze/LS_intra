f6e9055a885aa201b1c16f5615ef8a5369b34623
fabiozorze <fabio.zorze@hotmail.com>
2023-03-22 16:23:30 +0000

    Z_strategy positions np.nan

--- Strategy.py
+++ Strategy.py
@@ -15,7 +15,7 @@ def Z_score(df, lookback, long, short):
     df['long_entry'] = df['zScore'] < long
     df['long_exit'] = df['zScore'] > 0
 
-    df['positions_long'] = 0
+    df['positions_long'] = np.nan
     df.loc[df.long_entry, 'positions_long'] = 1
     df.loc[df.long_exit, 'positions_long'] = 0
     df.positions_long = df.positions_long.fillna(method='ffill')
@@ -24,7 +24,7 @@ def Z_score(df, lookback, long, short):
     df['short_entry'] = df['zScore'] > short
     df['short_exit'] = df['zScore'] < 0
 
-    df['positions_short'] = 0
+    df['positions_short'] = np.nan
     df.loc[df.short_entry, 'positions_short'] = -1
     df.loc[df.short_exit, 'positions_short'] = 0
 
